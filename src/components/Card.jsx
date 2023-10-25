22d05eb931f7906f6b0655356cc28d91e7a75d33
MakkonenGit <makkonengit22@gmail.com>
2023-10-24 19:51:33 -0600

    integrating an api to our project using useEffect

---
+++ src/components/Card.jsx
@@ -0,0 +1,43 @@
+function Card({ strMealThumb, strMeal, strInstructions }) {
+  return (
+    <>
+      <div className="card h-96 w-96 bg-base-100 bg-secondary text-primary-content shadow-xl">
+        <figure>
+          <img src={strMealThumb} alt={strMeal} />
+        </figure>
+        <div className="card-body">
+          <h2 className="card-title">{strMeal}</h2>
+          <p>
+            {strInstructions.substring(0, 150)}...
+            <button
+              className="font-bold hover:underline"
+              onClick={() => document.getElementById("my_modal_3").showModal()}
+            >
+              View more
+            </button>
+          </p>
+          <div className="card-actions justify-end">
+            <div className="badge badge-outline">Fashion</div>
+            <div className="badge badge-outline">Products</div>
+          </div>
+        </div>
+      </div>
+      {/* You can open the modal using document.getElementById('ID').showModal() method */}
+
+      <dialog id="my_modal_3" className="modal">
+        <div className="modal-box">
+          <form method="dialog">
+            {/* if there is a button in form, it will close the modal */}
+            <button className="btn btn-circle btn-ghost btn-sm absolute right-2 top-2">
+              ✕
+            </button>
+          </form>
+          <h3 className="text-lg font-bold">{strMeal}</h3>
+          <p className="py-4">{strInstructions}</p>
+        </div>
+      </dialog>
+    </>
+  );
+}
+
+export default Card;
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,5 +4,9 @@ export default {
   theme: {
     extend: {},
   },
-  plugins: [],
+  daisyui: {
+    themes: ["synthwave"],
+  },
+  plugins: [require('daisyui')],
 };
+
